02eea580f2dc24e38361a4c8fee310563f4f777e
liscju <piotr.listkiewicz@gmail.com>
2015-04-11 19:01:52 +0200

    Dodano skrypt do prostego uruchamiania

--- liscju/src/main.py
+++ liscju/src/main.py
@@ -1,25 +1,28 @@
 from node_managers import PiThreadMaster, PiThreadSlave
 from mpi4py import MPI
+from sys import argv
 
 __author__ = 'lee'
 MPI_ROOT = 0
 
-def start_workers(comm):
-    exp_count = 100
-    radius = 10
+def start_workers(comm,exp_count,radius):
     if comm.rank == MPI_ROOT:
         worker = PiThreadMaster(comm, exp_count, radius)
     else:
         worker = PiThreadSlave(comm, exp_count, radius)
     worker.execute()
 
-
 def initialize_communication():
     return MPI.COMM_WORLD
 
+# Return experiment_count,radius(of single pi calculation)
+def read_command_line_arguments():
+    return int(argv[1]),int(argv[2])
+
 def main():
     comm = initialize_communication()
-    start_workers(comm)
+    exp_count,radius = read_command_line_arguments()
+    start_workers(comm,exp_count,radius)
 
 if __name__ == '__main__':
     main()
---
+++ liscju/start.sh
@@ -0,0 +1,1 @@
+mpiexec -n 2 python src/main.py 100 10
