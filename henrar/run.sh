62561aa053d0ba30756dc6b3c23cdd191e1640da
Bartłomiej Kumor <aargh1@gmail.com>
2015-04-12 21:12:05 +0200

    Fixes for running stuff

--- henrar/run.sh
+++ henrar/run.sh
@@ -21,13 +21,13 @@ mpiexec -n 8 ./lab4.out 10000000 -b
 mpiexec -n 10 ./lab4.out 10000000 -b
 mpiexec -n 12 ./lab4.out 10000000 -b
 
-mpiexec -n 1 ./lab4.out 10000000 -b
-mpiexec -n 2 ./lab4.out 10000000 -b
-mpiexec -n 4 ./lab4.out 10000000 -b
-mpiexec -n 6 ./lab4.out 10000000 -b
-mpiexec -n 8 ./lab4.out 10000000 -b
-mpiexec -n 10 ./lab4.out 10000000 -b
-mpiexec -n 12 ./lab4.out 10000000 -b
+mpiexec -n 1 ./lab4.out 100000000 -b
+mpiexec -n 2 ./lab4.out 100000000 -b
+mpiexec -n 4 ./lab4.out 100000000 -b
+mpiexec -n 6 ./lab4.out 100000000 -b
+mpiexec -n 8 ./lab4.out 100000000 -b
+mpiexec -n 10 ./lab4.out 100000000 -b
+mpiexec -n 12 ./lab4.out 100000000 -b
 
 mpiexec -n 1 ./lab4.out 1000000000 -b
 mpiexec -n 2 ./lab4.out 1000000000 -b
@@ -53,13 +53,13 @@ mpiexec -n 8 ./lab4.out 10000000 -sc
 mpiexec -n 10 ./lab4.out 10000000 -sc
 mpiexec -n 12 ./lab4.out 10000000 -sc
 
-mpiexec -n 1 ./lab4.out 10000000 -sc
-mpiexec -n 2 ./lab4.out 10000000 -sc
-mpiexec -n 4 ./lab4.out 10000000 -sc
-mpiexec -n 6 ./lab4.out 10000000 -sc
-mpiexec -n 8 ./lab4.out 10000000 -sc
-mpiexec -n 10 ./lab4.out 10000000 -sc
-mpiexec -n 12 ./lab4.out 10000000 -sc
+mpiexec -n 1 ./lab4.out 100000000 -sc
+mpiexec -n 2 ./lab4.out 100000000 -sc
+mpiexec -n 4 ./lab4.out 100000000 -sc
+mpiexec -n 6 ./lab4.out 100000000 -sc
+mpiexec -n 8 ./lab4.out 100000000 -sc
+mpiexec -n 10 ./lab4.out 100000000 -sc
+mpiexec -n 12 ./lab4.out 100000000 -sc
 
 mpiexec -n 1 ./lab4.out 1000000000 -sc
 mpiexec -n 2 ./lab4.out 1000000000 -sc
