3d3d5dc92ad6ec0ed91b9a2c97bd34d32f130bbf
liscju <piotr.listkiewicz@gmail.com>
2015-04-12 15:47:03 +0200

    Lekki refaktoring

--- liscju/src/main.py
+++ liscju/src/main.py
@@ -1,6 +1,9 @@
 from sys import argv
+
 from mpi4py import MPI
-from commons import get_experiment_part_for_current_worker
+
+from commons import calculate_all_experiments_count, calculate_experiment_count_per_worker, ExperimentInfo
+
 from node_managers import PiThreadMasterWorker, PiThreadSlaveWorker, PiSequenceWorker
 
 
@@ -9,7 +12,7 @@ MPI_ROOT = 0
 
 
 class Main:
-    def __init__(self,calculate_starter_fun):
+    def __init__(self, calculate_starter_fun):
         self.func = calculate_starter_fun
 
     def main(self):
@@ -21,30 +24,27 @@ class Main:
     def read_command_line_arguments():
         return argv[1], int(argv[2]), int(argv[3])
 
-def start_sequence_worker(speedup_type, experiment_count,radius):
+
+def start_sequence_worker(speedup_type, experiment_count, radius):
     assert speedup_type == "basic"
-    worker = PiSequenceWorker(experiment_count,radius)
+    experiment_info = ExperimentInfo(False,speedup_type,1,experiment_count)
+    worker = PiSequenceWorker(experiment_count, radius, experiment_info)
     worker.execute()
 
-def start_concurrent_workers(speedup_type,experiment_count, radius):
+
+def start_concurrent_workers(speedup_type, experiment_count, radius):
     comm = MPI.COMM_WORLD
     worker_count = comm.size - 1
+    experiment_info = ExperimentInfo(True,speedup_type,worker_count,experiment_count)
     assert worker_count > 0
     if comm.rank == MPI_ROOT:
-        if speedup_type == "basic":
-            all_experiment_count = experiment_count
-        else:
-            assert speedup_type == "scale"
-            all_experiment_count = experiment_count * worker_count
-        worker = PiThreadMasterWorker(comm, all_experiment_count)
+        all_experiment_count = calculate_all_experiments_count(experiment_count, speedup_type, worker_count)
+        worker = PiThreadMasterWorker(comm, all_experiment_count, experiment_info)
     else:
-        if speedup_type == "basic":
-            worker_experiment_count = get_experiment_part_for_current_worker(comm.rank, worker_count, experiment_count)
-        else:
-            assert speedup_type == "scale"
-            worker_experiment_count = experiment_count
+        worker_experiment_count = calculate_experiment_count_per_worker(comm, experiment_count, speedup_type,
+                                                                        worker_count)
 
-        worker = PiThreadSlaveWorker(comm, worker_experiment_count, radius)
+        worker = PiThreadSlaveWorker(comm, worker_experiment_count, radius, experiment_info)
     worker.execute()
 
 
@@ -52,6 +52,7 @@ def usage():
     print "Usage: "
     print argv[0] + " {sequence|concurrent} {basic|scale} experiment_count radius_of_pi_calculation"
     exit(0)
+
 
 if __name__ == '__main__':
     prog_type = argv.pop(1)
